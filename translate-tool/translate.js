77c6cc620852925c5f3580f00f4efe061de1b1f5
Mathew Heard <splitice@users.noreply.github.com>
2022-06-24 17:06:33 +1000

    machine translate

---
+++ translate-tool/translate.js
@@ -0,0 +1,32 @@
+const translate = require('@iamtraction/google-translate');
+const source = require('../language/en.json')
+const fs = require('fs')
+const toLanguage = process.argv[1]
+
+async function translateObject(obj, result, key){
+    if(Array.isArray(obj)){
+        if(!result[key]) result[key] = []
+        for(const k of obj){
+            await translateObject(obj[k], result[key], result[key].length)
+        }
+        return
+    }
+    if(typeof obj === 'object'){
+        if(!result[key]) result[key] = {}
+        for(const k in obj){
+            await translateObject(obj[k], result[key], k)
+        }
+        return
+    }
+
+    const r = await translate(obj, {from: 'en', to: toLanguage})
+    result[key] = r.text
+}
+
+async function main(){
+    const result = {}
+    await translateObject(source, {root: result}, 'root')
+    fs.writeFileSync(`../language/${toLanguage}.json`, JSON.stringify(result), "utf8")
+}
+
+main()
